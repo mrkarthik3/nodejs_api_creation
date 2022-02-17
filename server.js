342e0254808acd6b5634f2efa1bd7d7b5c499c5f
Karthik <jskarthik1@gmail.com>
2022-02-17 17:13:39 +0530

    completed api building 101 with using only 'http' module of NodeJS

--- server.js
+++ server.js
@@ -1,13 +1,65 @@
 const http = require("http");
 
+const todos = [
+  { id: 1, text: "Todo One" },
+  { id: 2, text: "Todo Two" },
+  { id: 3, text: "Todo Three" },
+];
+
 const server = http.createServer((req, res) => {
-  //   console.log(req);
-  //   console.log(req.method); // prints method (GET/POST...) of incoming request
-  const { headers, url, method } = req; // getting these three properties via object destructuring
-  console.log(headers);
-  console.log(url);
-  console.log(method);
-  res.end();
+  const { method, url } = req;
+  let body = [];
+
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+
+      const response = {
+        success: false,
+        data: null,
+        error: null,
+      };
+
+      if (method == "GET" && url == "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method == "POST" && url == "/todos") {
+        const { id, text } = JSON.parse(body);
+
+        // if id or text is missing in the received todo..
+        // then send 400 response
+        if (!id || !text) {
+          status = 400;
+          response.error = "please add id and text";
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+
+      res.writeHead(status, {
+        "Content-Type": "application/json",
+        "X-Powered-By": "Node.js",
+      });
+
+      res.end(JSON.stringify(response));
+    });
+
+  // Below is shortcut way to write status code and headers
+
+  // If you have only one thing to send as response... you can mention that directly inside res.end()
+  // that will be sent as response and res.end() will end the response.
+
+  // In below, I am sending JSON.
+  // I took a plain object... converted it to JSON using JSON.stringify and sent that JSON as response.
 });
 
 const PORT = 5000;
